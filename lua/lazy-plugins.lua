bd488e6dbaf145dac5ff0aeeb8c6cb64155b9b49
Edwin Jones <19edwin.jones@gmail.com>
2025-03-22 18:07:09 -0400

    update configs

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,3 +1,12 @@
+  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
+  --    This is the easiest way to modularize your config.
+  --
+  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+local plugins = {
+  { import = 'custom.plugins' },
+  { import = 'custom.themes' },
+}
+
 -- [[ Configure and install plugins ]]
 --
 --  To check the current status of your plugins, run
@@ -9,7 +18,9 @@
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
-require('lazy').setup({
+-- Only load these plugins if not in VSCode
+if not vim.g.vscode then
+  vim.list_extend(plugins, {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
@@ -58,19 +69,15 @@ require('lazy').setup({
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
+})
+end
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  { import = 'custom.plugins' },
-  { import = 'custom.themes' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
-}, {
+require('lazy').setup(plugins, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
--- lua/options.lua
+++ lua/options.lua
@@ -61,4 +61,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.opt.tabstop = 4       -- Display tabs as 4 spaces
+vim.opt.shiftwidth = 4    -- Indentation size
+vim.opt.softtabstop = 4   -- Controls tab behavior in Insert mode
+
+
 -- vim: ts=2 sts=2 sw=2 et
