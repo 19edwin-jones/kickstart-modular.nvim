f43c0b95969ce494a23a17781fd86cf951e548a5
Edwin Jones <19edwin.jones@gmail.com>
2025-03-22 18:07:09 -0400

    update configs

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -38,11 +38,17 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
+---------------------
+-- Custom Keymaps ---
+---------------------
+
+vim.keymap.set('n', '<leader>o', ':put _<CR>', { noremap = true, silent = true }) -- New line below
+vim.keymap.set('n', '<leader>O', ':put! _<CR>', { noremap = true, silent = true }) -- New line above
 
 -- vim: ts=2 sts=2 sw=2 et
