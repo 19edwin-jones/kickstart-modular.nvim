49ab8dd1da6bc3b7c9d81921b84ad3f32d1b66ad
Edwin Jones <19edwin.jones@gmail.com>
2025-06-02 22:57:47 -0400

    Add new plugins

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,70 @@
+return {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+        local harpoon = require 'harpoon'
+        harpoon:setup {}
+
+        -- basic telescope configuration
+        local conf = require('telescope.config').values
+        local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+                table.insert(file_paths, item.value)
+            end
+
+            require('telescope.pickers')
+                .new({}, {
+                    prompt_title = 'Harpoon',
+                    finder = require('telescope.finders').new_table {
+                        results = file_paths,
+                    },
+                    previewer = conf.file_previewer {},
+                    sorter = conf.generic_sorter {},
+                })
+                :find()
+        end
+
+        vim.keymap.set('n', '<C-e>', function()
+            toggle_telescope(harpoon:list())
+        end, { desc = 'Open harpoon window' })
+
+        vim.keymap.set('n', '<leader>a', function()
+            harpoon:list():add()
+        end, { desc = 'Add current buffer to harpoon' })
+
+        -- Clear selected files from Harpoon list
+        vim.keymap.set('n', '<leader>r', function()
+            harpoon:list():remove()
+        end, { desc = 'Remove current buffer from harpoon' })
+
+        vim.keymap.set('n', '<C-1>', function()
+            harpoon:list():select(1)
+        end, { desc = 'Open harpoon buffer 1' })
+
+        vim.keymap.set('n', '<C-2>', function()
+            harpoon:list():select(2)
+        end, { desc = 'Open harpoon buffer 2' })
+
+        vim.keymap.set('n', '<C-3>', function()
+            harpoon:list():select(3)
+        end, { desc = 'Open harpoon buffer 3' })
+
+        vim.keymap.set('n', '<C-4>', function()
+            harpoon:list():select(4)
+        end, { desc = 'Open harpoon buffer 4' })
+
+        -- Toggle previous & next buffers stored within Harpoon list
+        vim.keymap.set('n', '<C-S-P>', function()
+            harpoon:list():prev()
+        end, { desc = 'Select previous buffer in harpoon' })
+
+        vim.keymap.set('n', '<C-S-N>', function()
+            harpoon:list():next()
+        end, { desc = 'Select next buffer in harpoon' })
+    end,
+}
