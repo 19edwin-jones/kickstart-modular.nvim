74c0096939771e5923e1f7075f3852fc37364c4d
Edwin Jones <19edwin.jones@gmail.com>
2025-06-06 18:50:52 -0400

    style: Format code

--- lua/custom/plugins/boole.lua
+++ lua/custom/plugins/boole.lua
@@ -1,25 +1,23 @@
-
 return {
-  {
-    'nat-418/boole.nvim',
-    event = 'VeryLazy', -- or 'InsertEnter', 'BufReadPre', etc. depending on when you want it loaded
-    config = function()
-      require('boole').setup({
-        mappings = {
-          increment = '<C-a>',
-          decrement = '<C-x>',
-        },
-        additions = {
-          { 'Foo', 'Bar' },
-          { 'tic', 'tac', 'toe' },
-        },
-        allow_caps_additions = {
-          { 'enable', 'disable' },
-          { 'true', 'false' },
-          { 'TODO', 'DONE' },
-        },
-      })
-    end,
-  },
+    {
+        'nat-418/boole.nvim',
+        event = 'VeryLazy', -- or 'InsertEnter', 'BufReadPre', etc. depending on when you want it loaded
+        config = function()
+            require('boole').setup {
+                mappings = {
+                    increment = '<C-a>',
+                    decrement = '<C-x>',
+                },
+                additions = {
+                    { 'Foo', 'Bar' },
+                    { 'tic', 'tac', 'toe' },
+                    { 'TODO', 'DONE' },
+                },
+                allow_caps_additions = {
+                    { 'enable', 'disable' },
+                    { 'true', 'false' },
+                },
+            }
+        end,
+    },
 }
-
