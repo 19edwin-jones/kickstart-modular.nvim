e35163b9d05cd5b1230042a6417c7c402092a362
Edwin Jones <19edwin.jones@gmail.com>
2025-07-19 18:16:48 -0400

    Add new plugins

---
+++ lua/custom/plugins/telescope-file-browser.lua
@@ -0,0 +1,14 @@
+return {
+    -- 'nvim-telescope/telescope-file-browser.nvim',
+    -- event = 'VeryLazy',
+    -- dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+    -- config = function()
+    --     vim.keymap.set('n', '<space>tfb', ':Telescope file_browser<CR>')
+    --
+    --     -- vim.keymap.set('n', '<space>tfb', ':Telescope file_browser path=%:p:h select_buffer=true<CR>')
+    --     --
+    --     -- vim.keymap.set('n', '<space>tfb', function()
+    --     --     require('telescope').extensions.file_browser.file_browser()
+    --     -- end)
+    -- end,
+}
